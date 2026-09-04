da5cb1fe72da4629ae052ff9812ee460044b232b
anish3565 <tripathianish12@gmail.com>
2026-09-04 17:31:17 +0530

    Data preprocessing using data from s3 bucket

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -8,7 +8,10 @@ import yaml
 import logging
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from src import logger
-# from src.connections import s3_connection
+from src.connections import s3_connection
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
@@ -74,9 +77,13 @@ def main():
         # test_size = params['data_ingestion']['test_size']
         test_size = 0.2
         
-        df = load_data(data_url='https://raw.githubusercontent.com/anish3565/Dataset/refs/heads/main/data.csv')
-        # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
-        # df = s3.fetch_file_from_s3("data.csv")
+        # df = load_data(data_url='https://raw.githubusercontent.com/anish3565/Dataset/refs/heads/main/data.csv')
+        s3 = s3_connection.s3_operations(
+            bucket_name=os.getenv("AWS_BUCKET_NAME"),
+            aws_access_key=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+        )
+        df = s3.fetch_file_from_s3("data.csv")
 
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
