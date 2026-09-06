6897ce7016346827831ade54593dc7cb7a89e66b
anish3565 <tripathianish12@gmail.com>
2026-09-06 14:24:42 +0530

    Data ingestion file set to use s3 bucket data

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -83,7 +83,7 @@ def main():
             aws_access_key=os.getenv("AWS_ACCESS_KEY_ID"),
             aws_secret_key=os.getenv("AWS_SECRET_ACCESS_KEY")
         )
-        df = s3.fetch_file_from_s3("data.csv")
+        df = s3.fetch_file_from_s3("data.csv") 
 
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
